5dcf2716b3aef35876ef550cc473e1470cfcf602
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2023-05-18 19:29:51 +0700

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Scripts is Downloading......"
+
+wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/shell/install_en.sh" && chmod 700 /root/install_en.sh && mv /root/install_en.sh /root/install.sh && /root/install.sh
+
+
+echo "Scripts Installed Sukses............"
+
+echo  " ====================================== "
+echo    subscribe & like my channel youtube
+echo      regard,,boychongzen aka x-root
+echo  " ====================================== "
+
