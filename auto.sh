7667621f4184c36bb0dc40ce305ca289de2fcc8c
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2023-05-18 19:32:30 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,14 +1,16 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 echo "Scripts is Downloading......"
-
+echo
+echo
 wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/shell/install_en.sh" && chmod 700 /root/install_en.sh && mv /root/install_en.sh /root/install.sh && /root/install.sh
-
-
+echo
+echo
 echo "Scripts Installed Sukses............"
-
+echo
 echo  " ====================================== "
-echo    subscribe & like my channel youtube
-echo      regard,,boychongzen aka x-root
+echo    Subscribe & Like My Channel Youtube
+echo  https://www.youtube.com/@AksesInternet2023    
+echo      Regard,,boychongzen aka x-root
 echo  " ====================================== "
 
