b8f164cea9845882deeb7fe3d301e1a5a2ce7eb6
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2023-05-21 11:08:36 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+# RUN INSTALLER FREE SCRIPT #
+# Author Boychongzen aka Xroot  #
 
-echo "Scripts is Downloading......"
+## echo "Scripts is Downloading......" ##
+sleep 1
 
 wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/shell/install_en.sh" && chmod 700 /root/install_en.sh && mv /root/install_en.sh /root/install.sh && /root/install.sh
 
 
-echo "Scripts Installed Sukses............"
+## echo "Scripts Installed Sukses............" ##
+sleep 1
 
-echo  " ====================================== "
-echo    subscribe & like my channel youtube
-echo      regard,,boychongzen aka x-root
-echo  " ====================================== "
+##  echo  " ====================================== " ## 
+              subscribe & like my channel youtube
+               Regard,,Boychongzen aka Xroot
+##  echo  " ====================================== " ## 
+sleep 1
 
